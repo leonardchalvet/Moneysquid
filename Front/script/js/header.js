ae01de4673f739ae06af4184cc7196fe1d86b9b3
Léonard <leonard@agence-me.com>
2020-04-20 17:30:07 +0200

    Change - Front - Header Desktop - Change Click to Hover

--- Front/script/js/header.js
+++ Front/script/js/header.js
@@ -1,4 +1,5 @@
 function headerDesktop_nav(){
+	/*
 	$('.common-header_desktop ul li').click(function(){
 		var thisIndex = $(this).index() + 1;
 
@@ -14,6 +15,23 @@ function headerDesktop_nav(){
 			$('.common-header_desktop .container-el .el:nth-child(' + thisIndex + ')').removeClass('show');
 		}
 		
+	})*/
+
+
+	$('.common-header_desktop .head ul li').mouseenter(function(){
+		var thisIndex = $(this).index() + 1;
+		$('.common-header_desktop .container-el .el:not(:nth-child(' + thisIndex + '))').removeClass('show');
+		if (!$('.common-header_desktop .container-el .el:nth-child(' + thisIndex + ')').hasClass('show')) {
+			$('.common-header_desktop .container-el').addClass('show');
+			$('.common-header_desktop').removeClass('style-white');
+			$('.common-header_desktop .container-el .el:nth-child(' + thisIndex + ')').addClass('show');
+		};
+	})
+
+	$('.common-header_desktop .container-el').mouseleave(function(){
+		$('.common-header_desktop .container-el').removeClass('show');
+		$('.common-header_desktop').addClass('style-white');
+		$('.common-header_desktop .container-el .el').removeClass('show');
 	})
 
 	$('.common-header_mobile .head .action-menu').click(function(){
