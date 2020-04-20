c27909dccc3f1d04bc47ccc3225dfcccd8049fb1
Alexis Prevost-Maynen <killer76520@gmail.com>
2020-04-20 15:39:13 +0200

    Change - Back - Log - Add username

--- Log/config.php
+++ Log/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$allUsers = [ 'leonard', 'alexis', 'arsalain', 'nadege', 'luigi', 'mickaël', 'etienne', 'pierre-alexandre' ];
+$allUsers = [ 'leonard', 'alexis', 'arsalain', 'nadege', 'luigi', 'mickaël', 'etienne', 'pierre-alexandre', 'mathieu' ];
 $realPassword = '$2y$10$ier72hu8D6fA1nO8FtlQNOGFoBWeJ1uOlq5ujt9E1wk/tG8QTXmQO';
 
 ?>
