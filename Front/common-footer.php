5ce98d2e9982653e984121f647b40f44cc401724
Léonard <leonard@agence-me.com>
2020-04-29 15:14:14 +0200

    Add - Front - Footer

--- Front/common-footer.php
+++ Front/common-footer.php
@@ -1,1 +1,123 @@
-<footer></footer>
+<footer>
+	<div class="wrapper">
+		<div class="head">
+			<a href="" class="logo">
+				<img src="img/common/logo-moneysquid-white.svg" alt="">
+			</a>
+			<p>
+				UN CRÉDIT VOUS ENGAGE ET DOIT ÊTRE REMBOURSÉ. VÉRIFIEZ VOS CAPACITÉS DE REMBOURSEMENT AVANT DE VOUS ENGAGER.
+			</p>
+		</div>
+		<div class="container-link">
+			<ul>
+				<li>
+					<h4>En savoir plus</h4>
+				</li>
+				<li>
+					<a href="">Qui somme-nous ?</a>
+				</li>
+				<li>
+					<a href="">Relation presse</a>
+				</li>
+			</ul>
+			<ul>
+				<li>
+					<h4>Contact</h4>
+				</li>
+				<li>
+					<a href="">Contactez-nous</a>
+				</li>
+				<li>
+					<a href="">Rejoignez-nous</a>
+				</li>
+			</ul>
+			<ul>
+				<li>
+					<h4>Informations</h4>
+				</li>
+				<li>
+					<a href="">Données personnelles</a>
+				</li>
+				<li>
+					<a href="">Plan du site</a>
+				</li>
+			</ul>
+			<ul>
+				<li>
+					<h4>Nous suivre</h4>
+				</li>
+				<li>
+					<div class="container-el">
+						<a href="" class="el">
+							<img src="img/footer/icn-linkedin.svg" alt="">
+						</a>
+						<a href="" class="el">
+							<img src="img/footer/icn-twitter.svg" alt="">
+						</a>
+						<a href="" class="el">
+							<img src="img/footer/icn-facebook.svg" alt="">
+						</a>
+						<a href="" class="el">
+							<img src="img/footer/icn-instagram.svg" alt="">
+						</a>
+					</div>
+				</li>
+			</ul>
+
+			<div class="container-trustpilot">
+				<div class="title">
+					<img src="img/footer/logo-strustpilot.svg" alt="">
+					<div class="container-stars">
+						<div class="star">
+							<img src="img/footer/icn-star.svg" alt="">
+						</div>
+						<div class="star">
+							<img src="img/footer/icn-star.svg" alt="">
+						</div>
+						<div class="star">
+							<img src="img/footer/icn-star.svg" alt="">
+						</div>
+						<div class="star">
+							<img src="img/footer/icn-star.svg" alt="">
+						</div>
+						<div class="star">
+							<img src="img/footer/icn-star.svg" alt="">
+						</div>
+					</div>
+				</div>
+				<p>
+					Noté excellent par plus de 3,000 utilisateurs !
+				</p>
+			</div>
+		</div>
+
+		<div class="container-ls">
+			<div class="container-el">
+				<a href="" class="el">
+					Mentions Légales
+				</a>
+				<a href="" class="el">
+					Copyright
+				</a>
+				<a href="" class="el">
+					Condition générale d’utilisation
+				</a>
+			</div>
+		</div>
+
+		<div class="container-note">
+			<p>
+				* Notre meilleur taux fixe négocié sur 20 ans, hors assurance constaté le 11/09/2019, sous réserve des conditions spécifiques de notre partenaire bancaire.
+				** Notre meilleur taux fixe négocié sur 12 mois, hors assurance constaté le 06/02/2019, sous réserve des conditions spécifiques de notre partenaire bancaire.
+				*** Baisse constatée sur un dossier de regroupement de crédits financé le 17 avril 2014 auprès de notre partenaire bancaire CFCAL. La diminution du montant des mensualités entraîne l'allongement de la durée de remboursement et majore le coût total du crédit. La réduction dépend de la durée restante des prêts rachetés.
+				(1) L'activité relative au courtage en crédit immobilier est certifiée ISO 9001 par AFNOR Certification sous le numéro 2016/71504.2.
+			</p>
+		</div>
+
+		<div class="container-alert">
+			<p>
+				<em>ALERTE PHISING</em> : Attention, vous pouvez être sollicités par de faux conseillers MoneySquid pour vous proposer des crédits, et vous demandant de transmettre des documents, des fonds, des coordonnées bancaires, etc. <em>SOYEZ VIGILANTS</em> : MoneySquid ne demande jamais à ses clients de virer sur ses comptes des sommes prêtées par les banques, à l’exception des honoraires des agences. Les conseillers MoneySquid vous écrivent toujours d’une adresse mail xxxx@moneysquid.com
+			</p>
+		</div>
+	</div>
+</footer>
