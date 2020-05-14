047c9d3a138d5912b9f2783c6fd685f6495cb370
Léonard <leonard@agence-me.com>
2020-05-14 16:43:10 +0200

    Add - Front - 4c Actualites Details

---
+++ Front/4c-actualites-details.php
@@ -0,0 +1,515 @@
+<html>
+	<head>
+
+		<title>4c - Actualités - Détails</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"/>
+
+		<link rel="stylesheet" type="text/css" href="style/css/style-page_actualites_details.css">
+
+	</head>
+	
+	<body>
+
+		<?php include('common-header.php') ?>
+
+		<main>
+
+			<div class="common-element-lightbox lightbox-newsletter">
+
+				<div class="background"></div>
+				
+				<div class="content">
+					<div class="cross">
+						<svg class="icn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 18">
+							<use xlink:href="img/common/icn-cross-1.svg#content"></use>
+						</svg>
+					</div>
+					<div class="container-lightbox">
+						<div class="illu">
+							<img src="img/4e-actualites-pop-in/illu-1.svg" alt="">
+						</div>
+						<p class="title">
+							Inscrivez-vous à la<br>newsletter mensuelle !
+						</p>
+						<p class="text">
+							Inscrivez-vous gratuitement à notre Newsletter et recevez une fois <br>par semaine nos conseils, astuces et offres flash. 
+						</p>
+						<div class="container-input">
+							<input type="email" placeholder="Votre email">
+							<a href="" class="btn">
+								<span class="btn-text">
+									Je m'inscris
+								</span>
+								<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7 10">
+									<use xlink:href="img/common/icn-arrow-2.svg#content"></use>
+								</svg>
+							</a>
+						</div>
+						<div class="container-cg">
+							<input type="checkbox">
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus habitasse quis eget ipsum sed nunc massa. Erat blandit mauris pharetra pulvinar ridiculus posuere. 
+							</p>
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+			<div class="common-element-lightbox lightbox-thanks">
+
+				<div class="background"></div>
+				
+				<div class="content">
+					<div class="cross">
+						<svg class="icn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 18">
+							<use xlink:href="img/common/icn-cross-1.svg#content"></use>
+						</svg>
+					</div>
+					<div class="container-lightbox">
+						<div class="illu">
+							<img src="img/4f-actualites-pop-in-2/illu-1.svg" alt="">
+						</div>
+						<p class="title">
+							Merci pour votre commentaire ! 
+						</p>
+						<p class="text">
+							Votre message à bien été pris en compte
+						</p>
+					</div>
+				</div>
+
+			</div>
+
+			<section class="section-cover">
+				
+				<div class="wrapper">
+					<div class="container-text">
+						<div class="container-path">
+							<ul>
+								<li>
+									<a href="">Accueil</a>
+									<svg class="icn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 6">
+										<use xlink:href="img/common/icn-arrow-1.svg#content"></use>
+									</svg>
+								</li>
+								<li>
+									<a href="">Actualités</a>
+									<svg class="icn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 6">
+										<use xlink:href="img/common/icn-arrow-1.svg#content"></use>
+									</svg>
+								</li>
+								<li>
+									<a href="">Actualité crédit</a>
+									<svg class="icn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 6">
+										<use xlink:href="img/common/icn-arrow-1.svg#content"></use>
+									</svg>
+								</li>
+								<li>
+									<a href="">Actualité du crédit immobilier</a>
+									<svg class="icn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 6">
+										<use xlink:href="img/common/icn-arrow-1.svg#content"></use>
+									</svg>
+								</li>
+							</ul>
+						</div>
+						<h1>
+							Définir Votre projet immobilier
+						</h1>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus habitasse quis eget ipsum sed nunc massa. Erat blandit mauris pharetra pulvinar ridiculus posuere. In imperdiet velit dapibus a erat morbi elementum, integer orci. Tincidunt mauris sed quam consectetur nisl porta sit. In dictum
+						</p>
+						<div class="container-author">
+							<div class="pp">
+								<img src="img/img-test/user.jpg" alt="">
+							</div>
+							<div class="text">
+								<div class="title">MS La Rédaction</div>
+								<div class="desc">Mis à jour le 02 Mai 2019  -  11:26</div>
+								<div class="infos">
+									<p>
+										Lecture <span>3 min</span>
+									</p>
+									<img src="img/4c-actualites_details/icn-read.svg" alt="">
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="container-img">
+						<img src="img/actualites/banner-1.png" alt="">
+						<p>Crédit photo © Name Crédit </p>
+					</div>
+				</div>
+			</section>
+
+			<section class="section-text">
+				<div class="wrapper">
+					<div class="container-action">
+						<div class="container-el">
+							<div class="el">
+								<div class="icn">
+									<img src="img/4c-actualites_details/icn-clap.png" alt="">
+									<img src="img/4c-actualites_details/icn-clap-fill.png" alt="">
+								</div>
+								<div class="text">
+									356
+								</div>
+							</div>
+							<div class="el">
+								<div class="icn">
+									<img src="img/4c-actualites_details/icn-bookmark.svg" alt="">
+									<img src="img/4c-actualites_details/icn-bookmark-fill.svg" alt="">
+								</div>
+							</div>
+							<div class="el">
+								<div class="icn">
+									<img src="img/4c-actualites_details/icn-facebook.svg" alt="">
+								</div>
+							</div>
+							<div class="el">
+								<div class="icn">
+									<img src="img/4c-actualites_details/icn-twitter.svg" alt="">
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="container-text">
+						<p>
+							Lorem ipsum <a href="">link</a> dolor sit amet,
+							<em>bold</em>  consectetur adipiscing elit. Placerat sit volutpat nec aliquam mattis. Placerat duis pellentesque maecenas dis feugiat. Arcu dui in nibh convallis purus convallis sapien. Urna cursus tortor quam mattis faucibus bibendum fermentum. Nibh elit, maecenas erat faucibus magna.
+						</p>
+						<h2>
+							Y-a-t-il eu une infraction à la loi Hoguet ?
+						</h2>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias impedit, modi non deserunt. Minus incidunt ipsam voluptatibus dignissimos eaque reprehenderit. Sed distinctio ratione ut dicta, ad ducimus, eius facere hic.
+						</p>
+						<h2>
+							Y-a-t-il eu une infraction à la loi Hoguet ?
+						</h2>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat sit volutpat nec aliquam mattis. Placerat duis pellentesque maecenas dis feugiat. Arcu dui in nibh convallis purus convallis sapien. Urna cursus tortor quam mattis faucibus bibendum fermentum. Nibh elit, maecenas erat faucibus magna.<br>
+							<br>
+							Luctus quam sagittis pharetra interdum sit aliquam pulvinar non laoreet. Curabitur nam ante purus at dictum. Blandit fringilla ornare mi, orci ac in. Sapien, quisque varius ornare lacus a interdum et donec. Sit arcu lorem eros, in nibh curabitur id adipiscing sed. Ac quis elementum diam massa egestas placerat odio. Eget viverra eget sapien nunc nunc, integer proin ultrices id. Mauris pretium mauris venenatis, elit parturient in iaculis lobortis nulla. Lorem ac amet amet mauris vitae, pulvinar porta diam.<br>
+							<br>
+							Non lectus mauris vitae, sed mi. Faucibus diam amet fermentum varius aliquam sed. Mi fringilla ante tincidunt placerat gravida arcu, risus, lacus. Hac in donec lorem auctor. Senectus leo enim, scelerisque sagittis id risus eget elit. Morbi interdum egestas donec et viverra tortor, sit lorem. Amet molestie eget mattis nibh. Mattis iaculis sed nulla elementum quam ultrices semper metus. Nisl, at lorem cursus non nibh. Pretium ultricies pulvinar donec elementum tortor in id diam. Habitasse vehicula imperdiet sed eros.
+							<ul>
+								<li>Text</li>
+								<li>Text</li>
+								<li>Text</li>
+								<li>Text</li>
+							</ul>
+							<ol>
+								<li>Text</li>
+								<li>Text</li>
+								<li>Text</li>
+								<li>Text</li>
+							</ol>
+							
+						</p>
+					</div>
+					<div class="sidebar">
+						<div class="container-cdr">
+							<div class="cdr">
+								<p class="title">
+									Crédit<br>Immobilier
+								</p>
+								<p class="desc">
+									Comparez les banques et obtenez le meilleur
+								</p>
+								<div class="btn">
+									<span class="btn-text">
+										Je compare
+									</span>
+									<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7 10">
+										<use xlink:href="img/common/icn-arrow-2.svg#content"></use>
+									</svg>
+								</div>
+							</div>
+							<div class="cdr">
+								<p class="title">
+									Crédit<br>Immobilier
+								</p>
+								<p class="desc">
+									Comparez les banques et obtenez le meilleur
+								</p>
+								<div class="btn">
+									<span class="btn-text">
+										Je compare
+									</span>
+									<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7 10">
+										<use xlink:href="img/common/icn-arrow-2.svg#content"></use>
+									</svg>
+								</div>
+							</div>
+						</div>
+						<div class="container-sommaire">
+							<div class="title"></div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="common-section_actu">
+				<div class="wrapper">
+					<h2>
+						<a href="#">Les actualités</a>
+					</h2>
+					<div class="container-el">
+						<div class="el">
+							<div class="content toHref" data-href="#">
+								<div class="cover">
+									<img src="img/common-section_actu/img-1.png" alt="">
+								</div>
+								<div class="container-text">
+									<h4>
+										<a href="#">Les Francais</a>
+									</h4>
+									<p>
+										Ut reprehenderit consectetur ipsum aliquip ea do voluptate enim do magna. Laborum est id magna exercitation est deserunt sit. 
+									</p>
+									<div class="link">
+										<span class="link-text">
+											Lire la suite
+										</span>
+										<svg class="link-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7 10">
+											<use xlink:href="img/common/icn-arrow-2.svg#content"></use>
+										</svg>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="el">
+							<div class="content toHref" data-href="#">
+								<div class="cover">
+									<img src="img/common-section_actu/img-2.png" alt="">
+								</div>
+								<div class="container-text">
+									<h4>
+										<a href="#">Facebook Pay</a>
+									</h4>
+									<p>
+										Ut reprehenderit consectetur ipsum aliquip ea do voluptate enim do magna. Laborum est id magna exercitation est deserunt sit. 
+									</p>
+									<div class="link">
+										<span class="link-text">
+											Lire la suite
+										</span>
+										<svg class="link-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7 10">
+											<use xlink:href="img/common/icn-arrow-2.svg#content"></use>
+										</svg>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="el">
+							<div class="content toHref" data-href="#">
+								<div class="cover">
+									<img src="img/common-section_actu/img-1.png" alt="">
+								</div>
+								<div class="container-text">
+									<h4>
+										<a href="#">Les Francais</a>
+									</h4>
+									<p>
+										Ut reprehenderit consectetur ipsum aliquip ea do voluptate enim do magna. Laborum est id magna exercitation est deserunt sit. 
+									</p>
+									<div class="link">
+										<span class="link-text">
+											Lire la suite
+										</span>
+										<svg class="link-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7 10">
+											<use xlink:href="img/common/icn-arrow-2.svg#content"></use>
+										</svg>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="el">
+							<div class="content toHref" data-href="#">
+								<div class="cover">
+									<img src="img/common-section_actu/img-2.png" alt="">
+								</div>
+								<div class="container-text">
+									<h4>
+										<a href="#">Facebook Pay</a>
+									</h4>
+									<p>
+										Ut reprehenderit consectetur ipsum aliquip ea do voluptate enim do magna. Laborum est id magna exercitation est deserunt sit. 
+									</p>
+									<div class="link">
+										<span class="link-text">
+											Lire la suite
+										</span>
+										<svg class="link-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7 10">
+											<use xlink:href="img/common/icn-arrow-2.svg#content"></use>
+										</svg>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="el">
+							<div class="content toHref" data-href="#">
+								<div class="cover">
+									<img src="img/common-section_actu/img-1.png" alt="">
+								</div>
+								<div class="container-text">
+									<h4>
+										<a href="#">Les Francais</a>
+									</h4>
+									<p>
+										Ut reprehenderit consectetur ipsum aliquip ea do voluptate enim do magna. Laborum est id magna exercitation est deserunt sit. 
+									</p>
+									<div class="link">
+										<span class="link-text">
+											Lire la suite
+										</span>
+										<svg class="link-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7 10">
+											<use xlink:href="img/common/icn-arrow-2.svg#content"></use>
+										</svg>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="el">
+							<div class="content toHref" data-href="#">
+								<div class="cover">
+									<img src="img/common-section_actu/img-2.png" alt="">
+								</div>
+								<div class="container-text">
+									<h4>
+										<a href="#">Facebook Pay</a>
+									</h4>
+									<p>
+										Ut reprehenderit consectetur ipsum aliquip ea do voluptate enim do magna. Laborum est id magna exercitation est deserunt sit. 
+									</p>
+									<div class="link">
+										<span class="link-text">
+											Lire la suite
+										</span>
+										<svg class="link-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7 10">
+											<use xlink:href="img/common/icn-arrow-2.svg#content"></use>
+										</svg>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="el">
+							<div class="content toHref" data-href="#">
+								<div class="cover">
+									<img src="img/common-section_actu/img-1.png" alt="">
+								</div>
+								<div class="container-text">
+									<h4>
+										<a href="#">Les Francais</a>
+									</h4>
+									<p>
+										Ut reprehenderit consectetur ipsum aliquip ea do voluptate enim do magna. Laborum est id magna exercitation est deserunt sit. 
+									</p>
+									<div class="link">
+										<span class="link-text">
+											Lire la suite
+										</span>
+										<svg class="link-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7 10">
+											<use xlink:href="img/common/icn-arrow-2.svg#content"></use>
+										</svg>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="el">
+							<div class="content toHref" data-href="#">
+								<div class="cover">
+									<img src="img/common-section_actu/img-2.png" alt="">
+								</div>
+								<div class="container-text">
+									<h4>
+										<a href="#">Facebook Pay</a>
+									</h4>
+									<p>
+										Ut reprehenderit consectetur ipsum aliquip ea do voluptate enim do magna. Laborum est id magna exercitation est deserunt sit. 
+									</p>
+									<div class="link">
+										<span class="link-text">
+											Lire la suite
+										</span>
+										<svg class="link-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7 10">
+											<use xlink:href="img/common/icn-arrow-2.svg#content"></use>
+										</svg>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="container-nav">
+						<div class="nav">
+							<svg class="nav-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7 10">
+								<use xlink:href="img/common/icn-arrow-2.svg#content"></use>
+							</svg>
+						</div>
+						<div class="nav">
+							<svg class="nav-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7 10">
+								<use xlink:href="img/common/icn-arrow-2.svg#content"></use>
+							</svg>
+						</div>
+
+						<div class="btn toHref" data-href="">
+							<span class="btn-text">Voir tous les avis clients</span>
+							<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7 10">
+								<use xlink:href="img/common/icn-arrow-2.svg#content"></use>
+							</svg>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="section-writecom">
+				<div class="wrapper">
+					<h2>Laissez un commentaire</h2>
+
+				</div>
+			</section>
+
+			<section class="common-section_newsletter">
+				<div class="wrapper">
+					<h2>
+						Inscrivez vous à la Newsletter hebdomadaire
+					</h2>
+					<div class="container">
+						<p>
+							Inscrivez-vous gratuitement à notre Newsletter et recevez une fois par semaine nos conseils, astuces et offres flash. 
+						</p>
+						<div class="container-input">
+							<input type="text" placeholder="Votre email">
+							<a href="" class="btn">
+								<span class="btn-text">
+									Je m'inscris
+								</span>
+								<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7 10">
+									<use xlink:href="img/common/icn-arrow-2.svg#content"></use>
+								</svg>
+							</a>
+						</div>
+						<div class="container-cg">
+							<input type="checkbox">
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus habitasse quis eget ipsum sed nunc massa. Erat blandit mauris pharetra pulvinar ridiculus posuere. 
+							</p>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		</main>
+
+		<?php include('common-footer.php') ?>
+
+		<script type="text/javascript" src="script/minify/js-page_actualites_details-min.js"></script>
+	</body>
+</html>
