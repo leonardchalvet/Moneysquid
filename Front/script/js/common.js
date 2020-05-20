f635c5137dc09018d5d2e3bae52fa906146323a7
Léonard <leonard@agence-me.com>
2020-05-20 10:44:40 +0200

    Change - Front - Btn scroll - Smooth scroll top

--- Front/script/js/common.js
+++ Front/script/js/common.js
@@ -20,7 +20,14 @@ window.addEventListener('scroll', function() {
 });
 
 document.querySelector("#common-btn_top").addEventListener('click', function() {
-	window.scrollTo(0, 0);
+    const scrollToTop = () => {
+      const c = document.documentElement.scrollTop || document.body.scrollTop;
+      if (c > 0) {
+        window.requestAnimationFrame(scrollToTop);
+        window.scrollTo(0, c - c / 8);
+      }
+    };
+    scrollToTop();
 });
 
 /*=======================================
